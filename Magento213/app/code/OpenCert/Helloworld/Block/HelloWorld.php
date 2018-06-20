4f1114994d9734036825f002f8fd5d2cbdcd2682
Bui Toan Thang <vietnga1910@gmail.com>
2018-06-20 11:30:49 +0700

    Test Session + Registry

--- Magento213/app/code/OpenCert/Helloworld/Block/HelloWorld.php
+++ Magento213/app/code/OpenCert/Helloworld/Block/HelloWorld.php
@@ -3,8 +3,31 @@ namespace OpenCert\Helloworld\Block;
  
 class Helloworld extends \Magento\Framework\View\Element\Template
 {
+    protected $_registry;
+	protected $_catalogSession;
+
+	public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Catalog\Model\Session $catalogSession,
+        array $data = []
+    ) {
+        $this->_registry = $registry;
+        $this->_catalogSession = $catalogSession;
+        parent::__construct($context, $data);
+   	}
     public function getHelloWorldTxt()
     {
+
         return 'Hello world!';
+    }
+    public function getCatalogSession() 
+    {
+        return $this->_catalogSession;
+    }
+
+     public function getRegistry() 
+    {
+        return $this->_registry->registry('custom_var');
     }
 }
